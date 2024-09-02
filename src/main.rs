e4e392cc5d972bbfd4925d7ba7353717ef0732dc
Javatrix <javatrix912@gmail.com>
2024-09-02 15:25:52 +0200

    Read input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::stdin;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Starting the add-on...");
     let username = std::env::var("USERNAME");
@@ -8,6 +10,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let username = username.unwrap();
 
     println!("Hello, {username}!");
+
+    let input = std::io::read_to_string(stdin());
+    if let Err(error) = input {
+        return Err(Box::new(error));
+    }
+    let input = input.unwrap();
+
+    println!("GOT THE INPUT!\n\t: {input}");
 
     Ok(())
 }
