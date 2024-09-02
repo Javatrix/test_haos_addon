7244a643838c5d9f6c1b786db810542b3dae7c27
Javatrix <javatrix912@gmail.com>
2024-09-02 14:57:31 +0200

    pls

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-USERNAME=$(jq -r '.username' /data/options.json)
+export USERNAME=$(jq -r '.username' /data/options.json)
 /build/target/release/test_addon
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Starting the add-on...");
     let username = std::env::var("USERNAME");
 
     if let Err(error) = username {
